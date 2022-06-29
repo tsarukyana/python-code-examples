859c15822931a5baecb030c383add07c321a7ea3
Arshak Tsarukyan <arshakt@gmail.com>
2022-06-29 15:56:22 +0400

    Avoiding Repetitive Property Methods

---
+++ avoiding_repetitive_property_methods.py
@@ -0,0 +1,60 @@
+"""
+You are writing classes where you are repeatedly having to define property methods that
+perform common tasks, such as type checking. You would like to simplify the code so
+there is not so much code repetition.
+"""
+
+
+# Consider a simple class where attributes are being wrapped by property methods:
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('name must be a string')
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int):
+            raise TypeError('age must be an int')
+        self._age = value
+
+
+# One possible approach is to make a function that simply defines the
+# property for you and returns it. For example:
+def typed_property(name, expected_type):
+    storage_name = '_' + name
+
+    @property
+    def prop(self):
+        return getattr(self, storage_name)
+
+    @prop.setter
+    def prop(self, value):
+        if not isinstance(value, expected_type):
+            raise TypeError('{} must be a {}'.format(name, expected_type))
+        setattr(self, storage_name, value)
+
+    return prop
+
+
+# Example use
+class Person2:
+    name = typed_property('name', str)
+    age = typed_property('age', int)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
